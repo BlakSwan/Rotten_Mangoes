239eb670b310475b57a6a0ea131a8d0bb1507cd3
Chris Jackson <chris.r.jackson78@gmail.com>
2016-02-06 22:53:51 +0000

    commit 17

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-
-  get 'reviews/create'
-
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'users/new'
-
-  get 'users/create'
 
   resources :movies do
     resources :reviews, only: [:new, :create]
@@ -18,7 +7,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
-
+  root to: 'movies#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
